c9c733a3e962b7251243c11b0e272a5231133dea
Mizuan Mohamed <mizuan.mohamed@gmail.com>
2026-04-23 22:09:00 +0500

    Disable registration and use custom logo

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,8 @@ use App\Http\Controllers\Admin\PageController;
 use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\Admin\TagController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
 
-Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
